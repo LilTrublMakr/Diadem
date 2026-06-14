c716e925661cfc93fa82fd8dce967a8e62d35504
LTM <ltm@ltm56.com>
2026-06-13 20:24:04 -0400

    dev config file

--- src/lib/services/config/config.server.ts
+++ src/lib/services/config/config.server.ts
@@ -2,7 +2,9 @@ import type { Config } from "@/lib/services/config/configTypes";
 import fs from "node:fs";
 import { parse } from "toml";
 
-const configFile = fs.readFileSync("./src/lib/server/config.toml", "utf8");
+const devPath = "./config/config.dev.toml";
+const defaultPath = "./src/lib/server/config.toml";
+const configFile = fs.readFileSync(fs.existsSync(devPath) ? devPath : defaultPath, "utf8");
 const config: Config = parse(configFile);
 
 export function getServerConfig() {
