3469c5125c73a5a05fbd2d0f27c813653c5daf77
LTM <ltm@ltm56.com>
2026-06-28 23:39:48 -0400

    fix docker build?

--- src/lib/server/db/stats.ts
+++ src/lib/server/db/stats.ts
@@ -1,18 +1,12 @@
 import mysql from 'mysql2/promise';
-import {
-	STATS_DB_HOST,
-	STATS_DB_PORT,
-	STATS_DB_USER,
-	STATS_DB_PASSWORD,
-	STATS_DB_NAME
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 const pool = mysql.createPool({
-	host: STATS_DB_HOST ?? '127.0.0.1',
-	port: Number(STATS_DB_PORT ?? 3306),
-	user: STATS_DB_USER ?? '',
-	password: STATS_DB_PASSWORD ?? '',
-	database: STATS_DB_NAME ?? 'pokemon_stats',
+	host: env.STATS_DB_HOST ?? '127.0.0.1',
+	port: Number(env.STATS_DB_PORT ?? 3306),
+	user: env.STATS_DB_USER ?? '',
+	password: env.STATS_DB_PASSWORD ?? '',
+	database: env.STATS_DB_NAME ?? 'pokemon_stats',
 	waitForConnections: true,
 	connectionLimit: 5
 });
